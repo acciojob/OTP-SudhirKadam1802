13e31c1c8dc9e5c72f9a45c1b02368c6d6b26509
Sudhir Kadam <156059651+SudhirKadam1802@users.noreply.github.com>
2026-02-23 15:52:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+let codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((input, index) => {
+
+  input.addEventListener("input", () => {
+
+    if (input.value !== "" && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+
+  });
+
+  input.addEventListener("keydown", (e) => {
+
+    if (e.key === "Backspace") {
+
+      if (input.value === "" && index > 0) {
+        codes[index - 1].focus();
+      }
+
+    }
+
+  });
+
+});
